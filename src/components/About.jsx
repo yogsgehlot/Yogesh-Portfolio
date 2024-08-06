825d5f604cb99c099afffef7428dc56d1294868f
Yogesh Gehlot <75558691+yogsgehlot@users.noreply.github.com>
2024-08-06 21:16:29 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,21 @@ import Navbar from "./Navbar";
 function About() {
   return (
     <div
-      className="flex flex-col justify-start items-center md:pt-10 pt-24 pr-10 pl-10 
-     bg-[#fdfffc]  h-screen"
+      className="flex flex-col justify-start  md:pt-10 pt-24 pr-10 pl-10 
+     bg-[#fdfffc]  h-screen "
     >
-      <div className="bg-[#d00000] md:px-4 py-1 flex justify-center ">
-        <p className="md:text-6xl text-4xl font-bold text-[#fdfffc]">ABOUT ME</p>
+      <div className="flex justify-center ">
+        <p className="md:text-6xl text-4xl font-bold bg-[#d00000] px-4 py-1  text-[#fdfffc]">
+          ABOUT ME
+        </p>
       </div>
       <div className="my-8">
-        <p className="md:text-4xl text-xl text-justify text-[#000000]">
+        <p className="md:text-4xl text-xl  text-[#000000]">
           I'm <b> Yogesh Gehlot</b>, Full-Stack Web Developer/Freelancer
         </p>
       </div>
       <div className="my-5">
-        <p className="text-2xl">
+        <p className="md:text-2xl text-[15px]">
           At the intersection of creativity and technology, I bring your ideas
           to life with a blend of frontend finesse and backend power. With a
           strong foundation in both aspects of web development, I specialize in
@@ -24,28 +26,30 @@ function About() {
         </p>
       </div>
 
-      <div className=" flex justify-center gap-x-5 pb-10 ">
-        <div className=" w-1/2 flex justify-center items-center flex-col gap-y-5 bg-[#000000] text-[#fdfffc] p-5 ">
-          <p className="text-4xl font-bold text-[#fdfffc] px-4 py-1">
+      <div className=" max-md:flex-col flex justify-center gap-x-5 pb-10 ">
+        <div className=" md:w-1/2 flex justify-center items-center flex-col gap-y-5 bg-[#000000] text-[#fdfffc] p-5 ">
+          <p className="md:text-4xl text-2xl font-bold text-[#fdfffc] px-4 py-1">
             My Mission
           </p>
-          <p className="text-xl">
+          <p className="md:text-xl text-[15px]">
             To provide high-quality, reliable, and innovative web solutions that
             meet the unique needs of each client. I believe in the power of
             collaboration and am committed to delivering projects that exceed
             expectations.
           </p>
         </div>
-        <div className=" w-1/2 flex justify-center items-center flex-col gap-y-5 p-5 ">
-          <div className="text-4xl font-bold bg-[#d00000] text-[#fdfffc] px-4 py-1">
+        <div className=" md:w-1/2 flex justify-center items-center flex-col gap-y-5 p-5 ">
+          <div className="md:text-4xl text-2xl font-bold bg-[#d00000] text-[#fdfffc] px-4 py-1">
             What I Do..?
           </div>
           <div>
             <div className="flex justify-center items-center gap-x-5">
-              <img src="/frontend.png" alt="" />
+              <img src="/frontend.png" className="" alt="" />
               <div className="flex flex-col justify-center items-start ">
-                <p className="text-2xl font-bold">Frontend Development</p>
-                <p className="text-[15px]">
+                <p className="md:text-2xl text-xl font-bold">
+                  Frontend Development
+                </p>
+                <p className="text-[15px] ">
                   Creating responsive, dynamic, and user-friendly interfaces
                   using the latest technologies.
                 </p>
@@ -54,7 +58,9 @@ function About() {
             <div className="flex justify-center items-center gap-x-5">
               <img src="/backend.png" alt="" />
               <div className="flex flex-col justify-center items-start ">
-                <p className="text-2xl font-bold">Backend Development</p>
+                <p className="md:text-2xl text-xl font-bold">
+                  Backend Development
+                </p>
                 <p className="text-[15px]">
                   Building secure, scalable, and efficient server-side logic
                   with databases that meet your project's needs.
@@ -64,7 +70,9 @@ function About() {
             <div className="flex justify-center items-center gap-x-5">
               <img src="/freelance.png" alt="" />
               <div className="flex flex-col justify-center items-start ">
-                <p className="text-2xl font-bold">Freelance Services</p>
+                <p className="md:text-2xl text-xl font-bold">
+                  Freelance Services
+                </p>
                 <p className="text-[15px]">
                   Offering flexible, client-focused solutions to help you bring
                   your projects to life on your terms.
